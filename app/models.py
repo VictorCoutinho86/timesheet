efe1bd28184a540fc9831adc2d7c18b35d482cbb
VictorCoutinho86 <victormelo@id.uff.br>
2018-01-23 18:16:39 -0200

    Iniciando configuração inicial do banco de dados.

--- app/models.py
+++ app/models.py
@@ -1,24 +1,21 @@
 from app import db, login
-import locale
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
-
 
 class Usuario(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    pis = db.Column(db.Integer, unique=True, index=True, required=True)
-    nome = db.Column(db.String(64), index=True, unique=True, required=True)
-    email = db.Column(db.String(120), index=True, unique=True, required=True)
-    username = db.Column(db.String(120, index=True, unique=True, required=True))
+    pis = db.Column(db.Integer, unique=True, index=True)
+    nome = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    username = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     perfil_colaborador = db.Column(db.String(15))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Usuário {}>'.format(self.nome)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -33,27 +30,26 @@ class Usuario(UserMixin, db.Model):
 
 class Ocorrencia(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    justificativa = db.Column(db.String(240), required=True)
-    data_ocorrencia = db.Column(db.Datetime)
-    hora_inicio = db.Column(db.Integer, required=True)
-    hora_fim - db.Column(db.Integer, required=True)
+    justificativa = db.Column(db.String(240))
+    data_ocorrencia = db.Column(db.DateTime)
+    hora_inicio = db.Column(db.Integer)
+    hora_fim = db.Column(db.Integer)
     id_colaborador = db.Column(db.Integer, db.ForeignKey('usuario.id'))
-    timestamp = db.Column(db.Datetime, default=datetime.utcnow())
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
 
 
 class Resposta(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    justificativa = db.Column(db.Integer, db.ForeignKey('ocorrencia.id'))
+    justificativa = db.Column(db.Integer, db.ForeignKey('ocorrencia.id'), primary_key=True)
     resposta = db.Column(db.String(240))
     status = db.Column(db.Boolean)
     id_gestor = db.Column(db.Integer, db.ForeignKey('usuario.id'))
-    timestamp = db.Column(db.Datetime, default=datetime.utcnow())
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
 
 
 class Integracao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     caminho = db.Column(db.String(240))
-    timestamp = db.Column(db.Datetime, default=datetime.utcnow())
-    # data_hora_ultimo_registro = db.Column(db.Datetime)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+    data_hora_ultimo_registro = db.Column(db.DateTime)
     id_colaborador = db.Column(db.Integer, db.ForeignKey('usuario.id'))
 
